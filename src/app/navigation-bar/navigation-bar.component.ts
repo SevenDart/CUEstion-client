bbf6c3bbeb086892cae4e439348c58c9427f947b
Pavel Slyaz <sir.sevendart@gmail.com>
2022-05-01 14:13:38 +0300

    reworked navigation-bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: [
-    './navigation-bar.component.css'
-  ]
+  styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
-
+  constructor(readonly router: Router) {
+  }
 }
